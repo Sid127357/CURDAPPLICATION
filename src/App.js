1b2d74828b371160945093de98446c14784634b3
Sidharth Kaushal <113352472+Sid127357@users.noreply.github.com>
2022-11-05 13:13:36 +0530

    adding some new files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import Navbaar from './components/Navbaar';
+import Home from './components/Home';
+import Resgister from './components/Resgister';
+import { Routes , Route} from 'react-router-dom'
+
 
 function App() {
   return (
     <>
-    hellow
+   <Navbaar/>
+   <Routes>
+    <Route  path='/' element = {<Home></Home>}/>
+    <Route  path='/register' element = {<Resgister></Resgister>}/>
+   </Routes>
     </>
   );
 }
---
+++ src/components/Resgister.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Resgister = () => {
+  return (
+    <>
+     <div className='container'>
+        <Link to = "/">Home</Link>
+     </div>
+
+    </>
+  )
+}
+
+export default Resgister;
